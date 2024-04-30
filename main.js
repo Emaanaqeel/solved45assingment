b5f71e3654e44abb1413e2b1ec051d8ceacf1b47
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-05-01 00:16:16 +0500

    project23

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 "use strict";
-/*22 Intentional Error: If you haven’t received an array index error in one of your
- programs yet, try to make one happen. Change an index in one of your programs to produce
-  an index error. Make sure you correct the error before closing the program.*/
-let myArray = ["ali", 24, "car", 12, "bus"];
-console.log(myArray[0]);
-console.log(myArray[3]);
-console.log(myArray[4]);
+/*23 Conditional Tests: Write a series of conditional tests. Print a statement describing
+each test and your prediction for the results of each test. Your code should look
+something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to
+ True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests
+evaluate to False.*/
+let myCar = "alto";
+console.log(myCar == "alto");
+//10 statements of true or false
+//1
+let myfruit = "Apple";
+console.log(myfruit == "apple");
+console.log(myfruit == "Apple");
+//2
+let myArry = [23, 3, 9];
+console.log(myArry.length == 3);
+console.log(myArry.length == 4);
+//3
+let stdname = "ali";
+console.log(stdname == "ali");
+console.log(stdname == "Ali");
+//4
+let myschool = "Government";
+console.log(myschool == "Government");
+console.log(myschool === "government");
+//5
+let mylang = "urdu";
+console.log(mylang == "urdu");
+console.log(mylang === "Urdu");
