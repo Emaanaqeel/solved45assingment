86595ad32dc3086f5ebf4810f3baf0a246036dbe
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-04-30 23:51:28 +0500

    project05

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the
-/*following, including the quotation marks://
+following, including the quotation marks:
 Albert Einstein once said, “A person who never made a mistake never tried anything new.”*/
-var myQuote = "albert einstein once said,\"A person who never made a mistake never tried anything new \" ";
-//backtick
-console.log(myQuote);
+var famousPerson = "albert einstein";
+var message = "once said, \"A  person who never made a mistake never tried anythind new \"";
+console.log(famousPerson + message);
--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
 /*Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the
-/*following, including the quotation marks://
+following, including the quotation marks:
 Albert Einstein once said, “A person who never made a mistake never tried anything new.”*/
 
-const myQuote : string = `albert einstein once said,"A person who never made a mistake never tried anything new " `
+const famousPerson : string ="albert einstein";
+const message : string = `once said, "A  person who never made a mistake never tried anythind new "`
 
-//backtick
-
-console.log(myQuote);
+console.log(famousPerson + message)
