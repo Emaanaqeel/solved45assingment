6a400031b025c7e8ef7c809e86959ab03723ad3a
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-05-01 00:18:02 +0500

    project25

--- main.ts
+++ main.ts
@@ -1,45 +1,14 @@
-/*24 More Conditional Tests: You don’t have to limit the number of tests you create to 10.
- If you want to try more comparisons, write more tests. Have at least one True and one 
- False result for each of the following:
-• Tests for equality and inequality with strings
+/*25 Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable 
+called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+• Write an if statement to test whether the alien’s color is green. If it is, print a
+ message that the player just earned 5 points.
 
-• Tests using the lower case function
+• Write one version of this program that passes the if test and another that fails.
+ (The version that fails will have no output.)*/
 
-• Numerical tests involving equality and inequality, greater than and less than,
- greater than or equal to, and less than or equal to
+ let alien_color = "green";
+ if(alien_color == "red"){
+    console.log(`player just earned 5 points`)
+ }else{
 
-• Tests using "and" and "or" operators
-
-• Test whether an item is in a array
-
-• Test whether an item is not in a array*/
-
-import { Console } from "console";
-import { toASCII } from "punycode";
-
-//equility and inequility
-let myName = "Ali";
-console.log(myName=="Ali")   //true equal to
-console.log(myName!=="ali") // true not equal to
-
-//numberic
-let myNumber = 10
-console.log(myNumber == 10) //true 
-console.log(myNumber!== 10) //false
-console.log(myNumber > 5)  //true    greater than
-console.log(myNumber < 5)  //false    less than
-console.log(myNumber <= 5)  //false    less than equal to
-console.log(myNumber >= 5)  //true    greater than equal to
- //and && ====or|| (pipe)
-
- let num1 = 10
- let num2 = 5
- console.log(num1 > 9 && num2 < 5); // false
-
- console.log(num1 > 9 || num2 < 5); //true
-
- let myArry = [2,4,"ali"]
- let mystring = "ali"
- console.log(Array.isArray(myArry));
- console.log(Array.isArray(mystring));
- 
+ }
