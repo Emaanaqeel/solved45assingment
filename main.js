c20b6a5bd2c1fd1157a5364d0fe7e898c5bc0432
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-04-30 23:58:57 +0500

    project08

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-/*7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result
- in the number 8. Be sure to enclose your operations in print statements to see the results.?*/
-var addition = 4 + 4;
-var subtraction = 10 - 2;
-var multiplication = 2 * 4;
-var division = 32 / 4;
-console.log(addition, subtraction, multiplication, division);
+"use strict";
+/*8. You should create four lines that look like this:
+___________________________________________
+
+console.log(5 + 3)
+_____________________________________________
+Your output should simply be four lines with the number 8 appearing once on each line.*/
+console.log(5 + 3);
+console.log(23 - 15);
+console.log(2 * 4);
+console.log(32 / 4);
