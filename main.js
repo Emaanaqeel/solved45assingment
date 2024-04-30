9c3dc42326b07453191691ed53d2a719f6bc5af2
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-05-01 00:15:10 +0500

    project22

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 "use strict";
-/*21 They think of something you could store in a TypeScript Object. Write a program that
- creates Objects containing these items.*/
-let studentDetail = {
-    name: "Erum",
-    age: 17,
-    gender: "female",
-    rollnumber: 12345,
-};
-console.log(studentDetail);
+/*22 Intentional Error: If you haven’t received an array index error in one of your
+ programs yet, try to make one happen. Change an index in one of your programs to produce
+  an index error. Make sure you correct the error before closing the program.*/
+let myArray = ["ali", 24, "car", 12, "bus"];
+console.log(myArray[0]);
+console.log(myArray[3]);
+console.log(myArray[4]);
