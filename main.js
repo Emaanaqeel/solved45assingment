8f12a2265d77b7d728dbc2ee1dfb1261b6b036af
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-04-30 23:48:00 +0500

    project03

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-var personName = "emaan";
-console.log("hello " + personName + " would you like to learn some python today");
+"use strict";
+/* 3 Name Cases: Store a person’s name in a variable, and then print that person’s name in
+ lowercase, uppercase, and titlecase.*/
+let personName = "shanayaMalik";
+//lowercase
+console.log(personName.toLowerCase());
+//uppercase
+console.log(personName.toUpperCase());
+//titlecase
+let firstLetter = personName.charAt(0).toUpperCase();
+let restLetter = personName.slice(1).toLowerCase();
+let titleCase = firstLetter + restLetter;
+console.log(titleCase);
--- main.ts
+++ main.ts
@@ -1,2 +1,21 @@
-let personName: string ="emaan";
-console.log("hello " + personName+ " would you like to learn some python today")
+/* 3 Name Cases: Store a person’s name in a variable, and then print that person’s name in
+ lowercase, uppercase, and titlecase.*/
+
+ let personName :string="shanayaMalik";
+
+ //lowercase
+
+ console.log(personName.toLowerCase());
+
+ //uppercase
+
+ console.log(personName.toUpperCase());
+
+ //titlecase
+
+ let firstLetter : string = personName .charAt(0).toUpperCase();
+ let restLetter : string = personName .slice(1).toLowerCase();
+
+ let titleCase = firstLetter+ restLetter;
+
+ console.log(titleCase);
