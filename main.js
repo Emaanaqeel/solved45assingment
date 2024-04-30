974bbd69f32a9b603e95fb6f4e1fe27a3839b044
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-05-01 00:01:02 +0500

    project10

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 "use strict";
-/*Favorite Number: Store your favorite number in a variable.
- Then, using that variable, create a message that reveals your favorite number. Print that message.*/
-let favNumber = 6;
-let message = `my favourite number is "${favNumber}"`;
-console.log(message);
+/*10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write
+because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence
+describing what the program does.*/
+let num1 = 23;
+let num2 = 5;
+// this will add num1 and num2 (this is comment in typescript)
+console.log(num1 + num2);
--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-/*Favorite Number: Store your favorite number in a variable.
- Then, using that variable, create a message that reveals your favorite number. Print that message.*/
+/*10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write
+because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence
+describing what the program does.*/
 
-let favNumber : number = 6;
-let message : string =`my favourite number is "${favNumber}"`;
+let num1 = 23;
+let num2= 5;
 
-console.log(message);
+// this will add num1 and num2 (this is comment in typescript)
+
+console.log(num1 + num2);
