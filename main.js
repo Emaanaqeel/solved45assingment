070ac3bc2d3d1094ef96455c4bffd8f2d8782e20
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-05-01 23:53:01 +0500

    project26

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 "use strict";
-/*25 Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable
-called alien_color and assign it a value of 'green', 'yellow', or 'red'.
-• Write an if statement to test whether the alien’s color is green. If it is, print a
- message that the player just earned 5 points.
+/*26 Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an
+if-else chain.
+• If the alien’s color is green, print a statement that the player just earned 5 point
+ for shooting the alien.
 
-• Write one version of this program that passes the if test and another that fails.
- (The version that fails will have no output.)*/
-let alien_color = "green";
-if (alien_color == "red") {
+• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+• Write one version of this program that runs the if block and another that runs the else
+ block.*/
+let aliencolor = "green";
+if (aliencolor == "green") {
     console.log(`player just earned 5 points`);
 }
 else {
+    console.log(`player just earned 10 points `);
+}
+//else statement
+let alien_color = "green";
+if (aliencolor == "red") {
+    console.log(`player just earned 5 points`);
+}
+else {
+    console.log(`player just earned 10 points `);
 }
