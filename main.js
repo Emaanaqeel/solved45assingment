d50bdd5ffea6c81807768245cf10a2d467f80e85
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-05-02 00:01:59 +0500

    project36

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 "use strict";
-/*35 Animals: Think of at least three different animals that have a common characteristic.
- Store the names of these animals in a list, and then use a for loop to print out the name
-  of each animal. • Modify your program to print a statement about each animal, such as A
-  dog would make a great pet. • Add a line at the end of your program stating what these
-  animals have in common. You could print a sentence such as Any of these animals would
-   make a great pet!*/
-let animalnames = ["goat", "cow", "sheep", "camel"];
-for (let i of animalnames) {
-    console.log([i]);
+/* 36 T-Shirt: Write a function called make_shirt() that accepts a size and the text of a
+ message that should be printed on the shirt. The function should print a sentence
+ summarizing the size of the shirt and the message printed on it. Call the functon*/
+function make_shirt(size, label) {
+    //return size +label
+    return `my shirt size is ${size} and ${label}`;
 }
-for (let k of animalnames) {
-    console.log(`${k} is a domestic animal`);
-}
-console.log(`\nAll these animals  ${animalnames[0]},${animalnames[1]},${animalnames[2]},and ${animalnames[3]} meat is halal in islam \n`);
+let myfun = make_shirt(36, " The sunny day");
+console.log(myfun);
+let myfun2 = (size, label) => {
+    return size + label;
+};
+console.log(myfun2(36, "the cool day"));
--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-/*35 Animals: Think of at least three different animals that have a common characteristic.
- Store the names of these animals in a list, and then use a for loop to print out the name
-  of each animal. • Modify your program to print a statement about each animal, such as A 
-  dog would make a great pet. • Add a line at the end of your program stating what these 
-  animals have in common. You could print a sentence such as Any of these animals would
-   make a great pet!*/
+/* 36 T-Shirt: Write a function called make_shirt() that accepts a size and the text of a
+ message that should be printed on the shirt. The function should print a sentence 
+ summarizing the size of the shirt and the message printed on it. Call the functon*/
 
-   let animalnames = ["goat","cow","sheep","camel"];
+ function make_shirt(size :number , label:string){
+    //return size +label
+    return `my shirt size is ${size} and ${label}`
+ }
+ let myfun = make_shirt( 36, " The sunny day")
 
-   for(let i of animalnames){
-    console.log([i]);
-   }
-   for(let k of animalnames){
-    console.log(`${k} is a domestic animal`)
-   }
-   console.log(`\nAll these animals  ${animalnames[0]},${animalnames[1]},${animalnames[2]},and ${animalnames[3]} meat is halal in islam \n`)
+ console.log(myfun);
+
+ /*let myfun2 = (size : number, label :string)=>{
+    return size +label
+ }
+ console.log(myfun2(36 ," The cool day"))*/
