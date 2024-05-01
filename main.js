163dabdc43c1f6431eb9f5647a02d604efef5709
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-05-02 00:07:29 +0500

    project41

--- main.js
+++ main.js
@@ -1,28 +1,10 @@
 "use strict";
-/*40 Album: Write a function called make_album() that builds a Object describing a music
-album. The function should take in an artist name and an album title, and it should return
- a Object containing these two pieces of information. Use the function to make three
- dictionaries representing different albums. Print each return value to show that Objects
-  are storing the album information correctly. Add an optional parameter to make_album()
- that allows you to store the number of tracks on an album. If the calling line includes
- a value for the number of tracks, add that value to the album’s Object. Make at least one
-  new function call that includes the number of tracks on an album.*/
-function make_album(artistname, albumTitle) {
-    return { artistname, albumTitle };
+/*41 Magicians: Make a array of magician’s names. Pass the array to a function called
+ show_magicians(), which prints the name of each magician in the array.*/
+let magiciannames = ["moosa", "pehlaj", "subhan", "aqeel"];
+function show_magicians() {
+    for (let magicianname of magiciannames) {
+        console.log(magicianname);
+    }
 }
-let album1 = make_album("ali", "rang-e-mohabat");
-let album2 = make_album("Madad", "roshan andhera");
-let album3 = make_album("ayaz", "mausam -e- dil");
-console.log(album1);
-console.log(album2);
-console.log(album3);
-//number of tracks
-function make_album2(artistname, albumTitle, numberoftracks) {
-    return { artistname, albumTitle, numberoftracks };
-}
-let album4 = make_album2("ali", "rang-e-mohabat", 30);
-let album5 = make_album2("Madad", "roshan andhera", 55);
-let album6 = make_album2("ayaz", "mausam -e- dil");
-console.log(album4);
-console.log(album5);
-console.log(album6);
+show_magicians();
