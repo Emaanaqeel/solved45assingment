3bde1b1c1eff340967a8b459a90b6eda6900fa15
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-05-02 00:09:33 +0500

    project43

--- main.ts
+++ main.ts
@@ -1,17 +1,25 @@
-/*42 reat Magicians: Start with a copy of your program from Exercise 39. Write a function 
-called make_great() that modifies the array of magicians by adding the phrase the Great to
- each magician’s name. Call show_magicians() to see that the list has actually been
-  modified.*/
+/*43 Unchanged Magicians: Start with your work from Exercise 40. Call the function
+ make_great() with a copy of the array of magicians’ names. Because the original array 
+ will be unchanged, return the new array and store it in a separate array. Call 
+ show_magicians() with each array to show that you have one array of the original names
+  and one array with the Great added to each magician’s name.*/
 
+  let magiciannames2 =["moosa","pehlaj","subhan","aqeel"]
 
+//making a copy of array
 
- let magiciannames =["moosa","pehlaj","subhan","aqeel"]
-
+let magiciannamescopy =[...magiciannames2]
 
  function show_magicians(greet:string){
-    for(let item of magiciannames){
-        console.log(greet ,item);
+    let wihtgreetings ="";
+    for(let item of magiciannames2){
+        wihtgreetings +=`${greet} ${item}\n`
     }
+        return wihtgreetings;
+    
  }
-show_magicians("hello, how are you Mr.");
-show_magicians("hello ");
+
+let mygreetings = show_magicians("hello ");
+let makearray = mygreetings.split('\n')
+console.log(makearray);
+console.log(magiciannames2);
