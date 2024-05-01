9e6b40f1ac269b31285bb38c3e6cd0f24d1600eb
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-05-02 00:05:56 +0500

    project40

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 "use strict";
-/*39 City Names: Write a function called city_country() that takes in the name of a city
-and its country. The function should return a string formatted like this:
-
-"Lahore, Pakistan"
-
-Call your function with at least three city-country pairs, and print the value that’s
-returned.*/
-function city_country(city, country) {
-    return `"${city}, ${country}"`;
+/*40 Album: Write a function called make_album() that builds a Object describing a music
+album. The function should take in an artist name and an album title, and it should return
+ a Object containing these two pieces of information. Use the function to make three
+ dictionaries representing different albums. Print each return value to show that Objects
+  are storing the album information correctly. Add an optional parameter to make_album()
+ that allows you to store the number of tracks on an album. If the calling line includes
+ a value for the number of tracks, add that value to the album’s Object. Make at least one
+  new function call that includes the number of tracks on an album.*/
+function make_album(artistname, albumTitle) {
+    return { artistname, albumTitle };
 }
-console.log(city_country("karachi", " pakistan"));
-console.log(city_country("dubai", " UAE"));
-console.log(city_country("london", " united kingdom"));
-console.log(city_country("riyaz", " saudi arabia"));
+let album1 = make_album("ali", "rang-e-mohabat");
+let album2 = make_album("Madad", "roshan andhera");
+let album3 = make_album("ayaz", "mausam -e- dil");
+console.log(album1);
+console.log(album2);
+console.log(album3);
+//number of tracks
+function make_album2(artistname, albumTitle, numberoftracks) {
+    return { artistname, albumTitle, numberoftracks };
+}
+let album4 = make_album2("ali", "rang-e-mohabat", 30);
+let album5 = make_album2("Madad", "roshan andhera", 55);
+let album6 = make_album2("ayaz", "mausam -e- dil");
+console.log(album4);
+console.log(album5);
+console.log(album6);
--- main.ts
+++ main.ts
@@ -1,16 +1,32 @@
-/*39 City Names: Write a function called city_country() that takes in the name of a city 
-and its country. The function should return a string formatted like this:
+/*40 Album: Write a function called make_album() that builds a Object describing a music 
+album. The function should take in an artist name and an album title, and it should return
+ a Object containing these two pieces of information. Use the function to make three 
+ dictionaries representing different albums. Print each return value to show that Objects
+  are storing the album information correctly. Add an optional parameter to make_album()
+ that allows you to store the number of tracks on an album. If the calling line includes
+ a value for the number of tracks, add that value to the album’s Object. Make at least one
+  new function call that includes the number of tracks on an album.*/
 
-"Lahore, Pakistan"
+ function make_album(artistname :string , albumTitle :string){
+    return{artistname,albumTitle}
+  }
+  let album1 = make_album("ali","rang-e-mohabat");
+  let album2 = make_album("Madad","roshan andhera");
+  let album3 = make_album("ayaz","mausam -e- dil");
 
-Call your function with at least three city-country pairs, and print the value that’s 
-returned.*/
+  console.log(album1);
+  console.log(album2);
+  console.log(album3);
 
-function city_country(city :string ,country: string){
-    return`"${city}, ${country}"`
+  //number of tracks
 
-}
-console.log(city_country("karachi"," pakistan"))
-console.log(city_country("dubai"," UAE"))
-console.log(city_country("london"," united kingdom"))
-console.log(city_country("riyaz"," saudi arabia"))
+  function make_album2(artistname :string , albumTitle :string ,numberoftracks ?:number ){
+    return{artistname,albumTitle,numberoftracks}
+  }
+  let album4 = make_album2("ali","rang-e-mohabat",30);
+  let album5 = make_album2("Madad","roshan andhera",55);
+  let album6 = make_album2("ayaz","mausam -e- dil");
+
+  console.log(album4);
+  console.log(album5);
+  console.log(album6);
