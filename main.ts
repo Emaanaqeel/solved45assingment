691853405c4ebd4f00266081cc1edc3809466d03
Emaanaqeel <167984241+Emaanaqeel@users.noreply.github.com>
2024-05-02 00:00:19 +0500

    project34

--- main.ts
+++ main.ts
@@ -1,29 +1,22 @@
-/* 33 Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or
- 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
-• Store the numbers 1 through 9 in a array.
+/*34 Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names
+ in a array, and then use a for loop to print the name of each pizza.
+• Modify your for loop to print a sentence using the name of the pizza instead of printing
+ just the name of the pizza. For each pizza you should have one line of output containing
+  a simple statement like I like pepperoni pizza.
 
-• Loop through the array.
+• Add a line at the end of your program, outside the for loop, that states how much you 
+like pizza. The output should consist of three or more lines about the kinds of pizza you
+ like and then an additional sentence, such as I really love pizza!*/
 
-• Use an if-else chain inside the loop to print the proper ordinal ending for each number.
- Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be
-  on a separate line.*/
+ let favorite_pizza = ["corn pizza","chicken tikka pizza","white sauce pizza"];
+//printing only pizza names
 
-  let myNumber = [1,2,3,4,5,6,7,8,9,];
-  //for loop
+ for(let i = 0 ;i< favorite_pizza.length ;i++){
+      console.log(favorite_pizza[i])
+ }
+ //printing pizza name with message
 
-  for(let i =0 ; i < myNumber.length ;i++){
-
-    //we can use conditions
-    if(myNumber [i]== 1){
-        console.log(`${myNumber[i]}st`)
-    }
-    else if(myNumber [i]== 2){
-        console.log(`${myNumber[i]}nd`)
-    }
-    else if(myNumber [i]== 3){
-        console.log(`${myNumber[i]}rd`)
-    }
-    else if(myNumber [i]>= 4 && myNumber[i] <= 9){
-        console.log(`${myNumber[i]}th`)
-    }
-  }
+ for(let i = 0 ;i< favorite_pizza.length ;i++){
+    console.log(`\nI like to eat ${favorite_pizza[i]}`)
+ }
+ console.log(`\nI really like pizza , because it comes in different flavor and toppings \n`)
